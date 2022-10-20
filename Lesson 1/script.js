0b203d7cc83a9ded40256249c7bf30b0e8c68911
Ready32 <amelin-free@yandex.ru>
2022-10-21 00:10:04 +0300

    done lesson 1

---
+++ Lesson 1/script.js
@@ -0,0 +1,17 @@
+// Задача 1
+var Temperature = 30;
+alert((9 / 5) * Temperature + 32);
+
+// Задача 2
+var admin;
+var name = 'Василий';
+admin = name;
+alert(admin);
+
+// Задача 3
+
+alert(1000 + '108');
+
+// Задача 4
+
+//script (async и defer).
